8a937eedc84f555c2a2cccaf3fc90cf0c4c94a78
Rodrigo Duarte Louro <digao.louro@gmail.com>
2014-05-15 12:42:08 -0300

    Soma e testes feitos

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,4 +7,11 @@ package main;
 
 public class Main {
 	
+	public static void main(String[] args) {
+		
+	}
+	
+	public static int soma (int a, int b) {
+		return a + b; 
+	}
 }
--- src/teste/MainTest.java
+++ src/teste/MainTest.java
@@ -6,14 +6,16 @@
 package teste;
 
 import static org.junit.Assert.*;
+import main.Main;
 
 import org.junit.Test;
 
 public class MainTest {
 
 	@Test
-	public void test() {
-		fail("dsadsademented");
+	public void somaTest() {
+		assertEquals(5, Main.soma(2, 3));
+		assertEquals(-1, Main.soma(2, -3));
+		assertEquals(0, Main.soma(0, 0));
 	}
-
 }
