17be71a681a7dfced68eeed944edfee7637f460b
Rodrigo Duarte Louro <digao.louro@gmail.com>
2014-05-15 12:31:08 -0300

    Commit inicial

---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author Rodrigo Duarte Louro
+ *
+ * May 15, 2014
+ */
+package main;
+
+public class Main {
+	
+}
---
+++ src/teste/MainTest.java
@@ -0,0 +1,19 @@
+/**
+ * @author Rodrigo Duarte Louro
+ *
+ * May 15, 2014
+ */
+package teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
